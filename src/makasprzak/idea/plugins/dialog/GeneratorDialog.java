0929c80f3c012f1f9354675c64476dcdfaa722f5
makasprzak <mail.maciej.kasprzak@gmail.com>
2015-03-26 19:30:58 +0100

    #4 JDK 6 support

--- src/makasprzak/idea/plugins/dialog/GeneratorDialog.java
+++ src/makasprzak/idea/plugins/dialog/GeneratorDialog.java
@@ -1,6 +1,7 @@
 package makasprzak.idea.plugins.dialog;
 
 import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.LabeledComponent;
 import com.intellij.psi.PsiClass;
@@ -11,18 +12,19 @@ import makasprzak.idea.plugins.model.Property;
 import makasprzak.idea.plugins.model.PsiPropertyContainer;
 
 import javax.swing.*;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.google.common.collect.Lists.transform;
 
 public class GeneratorDialog extends DialogWrapper{
-    private JList<PsiPropertyContainer> properties;
+    private JList properties;
     private final LabeledComponent<JPanel> component;
 
     protected GeneratorDialog(PsiClass psiClass, List<PsiPropertyContainer> allProperties) {
         super(psiClass.getProject());
         setTitle("Configure Step Builder");
-        this.properties = new JBList(new CollectionListModel<>(allProperties));
+        this.properties = new JBList(new CollectionListModel<PsiPropertyContainer>(allProperties));
         this.properties.setCellRenderer(new PropertyCellRenderer());
         this.properties.setSelectedIndices(range(allProperties.size()));
         ToolbarDecorator decorator = ToolbarDecorator.createDecorator(properties);
@@ -48,7 +50,18 @@ public class GeneratorDialog extends DialogWrapper{
     }
 
     public List<Property> getProperties() {
-        return transform(properties.getSelectedValuesList(), toProperty());
+        return transform(getPsiPropertyContainers(), toProperty());
+    }
+
+    private List<PsiPropertyContainer> getPsiPropertyContainers() {
+        Object[] selectedObjects = properties.getSelectedValues();
+        List<Object> selectedObjectList = Arrays.asList(selectedObjects);
+        return Lists.transform(selectedObjectList, new Function<Object, PsiPropertyContainer>() {
+            @Override
+            public PsiPropertyContainer apply(Object o) {
+                return (PsiPropertyContainer) o;
+            }
+        });
     }
 
     private Function<PsiPropertyContainer, Property> toProperty() {
--- src/makasprzak/idea/plugins/dialog/PropertyCellRenderer.java
+++ src/makasprzak/idea/plugins/dialog/PropertyCellRenderer.java
@@ -1,20 +1,25 @@
 package makasprzak.idea.plugins.dialog;
 
 import com.intellij.ide.util.DefaultPsiElementCellRenderer;
+import com.intellij.psi.PsiElement;
 import makasprzak.idea.plugins.model.PsiPropertyContainer;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class PropertyCellRenderer implements ListCellRenderer<PsiPropertyContainer> {
+public class PropertyCellRenderer implements ListCellRenderer {
     private final DefaultPsiElementCellRenderer renderer = new DefaultPsiElementCellRenderer();
 
     @Override
-    public Component getListCellRendererComponent(JList<? extends PsiPropertyContainer> list,
-                                                  PsiPropertyContainer value,
+    public Component getListCellRendererComponent(JList list,
+                                                  Object value,
                                                   int index,
                                                   boolean isSelected,
                                                   boolean cellHasFocus) {
-        return renderer.getListCellRendererComponent(list,value.getPsiElement(),index,isSelected,cellHasFocus);
+        return renderer.getListCellRendererComponent(list, getValue(value),index,isSelected,cellHasFocus);
+    }
+
+    private PsiElement getValue(Object value) {
+        return ((PsiPropertyContainer)value).getPsiElement();
     }
 }
